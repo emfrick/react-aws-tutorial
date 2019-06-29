9723a16b6b9534418744b4f473df4a432fb4c9bd
Eric Frick <eric.frick@icloud.com>
2019-06-28 22:21:02 -0400

    Styled landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
-
 import { observer } from 'mobx-react'
+import { Container, Header, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+import * as ROUTES from '../../constants/routes'
 import { withStore } from '../../store';
 
 const LandingPage = observer((props) => (
-    <h1>{props.store.tagLine}</h1>
+    <Container text>
+        <Header as='h1' content='Welcome to React/AWS'
+        style={{
+            fontSize: '4em',
+            fontWeight: 'normal',
+            marginBottom: 0,
+            marginTop: '3em',
+        }}
+        textAlign='center'
+        />
+        <Header as='h3' textAlign='center'>
+            <Link to={ROUTES.SIGN_IN}>Sign in to get started</Link>
+        </Header>
+    </Container>
 ))
 
 export default withStore(LandingPage)
